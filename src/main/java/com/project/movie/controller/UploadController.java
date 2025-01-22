96c3e9a1be58ab8e173e1c2e1a15bc8c157f84bf
kyung <kyung5896@gmail.com>
2025-01-23 00:38:20 +0900

    코드 수정

--- src/main/java/com/project/movie/controller/UploadController.java
+++ src/main/java/com/project/movie/controller/UploadController.java
@@ -24,17 +24,18 @@ import java.util.List;
 import java.util.UUID;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.util.FileCopyUtils;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 
 @Log4j2
 @Controller
 @RequestMapping("/upload")
+@PropertySource(value = "application.properties")
 public class UploadController {
 
     // application.properties 에 있는 값
